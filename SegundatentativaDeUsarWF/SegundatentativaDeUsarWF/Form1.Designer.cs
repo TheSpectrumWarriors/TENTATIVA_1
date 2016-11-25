f68f9fe6425b7046e8113a013d4146e744cec17a
Joao gomes <joaosantigomes@gmail.com>
2016-11-25 16:30:12 +0000

    Base da Calculadora

---
+++ SegundatentativaDeUsarWF/SegundatentativaDeUsarWF/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace SegundatentativaDeUsarWF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BT7 = new System.Windows.Forms.Button();
+            this.BT9 = new System.Windows.Forms.Button();
+            this.BT8 = new System.Windows.Forms.Button();
+            this.BTMais = new System.Windows.Forms.Button();
+            this.BTMenos = new System.Windows.Forms.Button();
+            this.BT5 = new System.Windows.Forms.Button();
+            this.BT6 = new System.Windows.Forms.Button();
+            this.BT4 = new System.Windows.Forms.Button();
+            this.BTDividir = new System.Windows.Forms.Button();
+            this.BT2 = new System.Windows.Forms.Button();
+            this.BT3 = new System.Windows.Forms.Button();
+            this.BT1 = new System.Windows.Forms.Button();
+            this.BTMultiplicar = new System.Windows.Forms.Button();
+            this.BT0 = new System.Windows.Forms.Button();
+            this.BTIgual = new System.Windows.Forms.Button();
+            this.BTLimpar = new System.Windows.Forms.Button();
+            this.mtbResultadao = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // BT7
+            // 
+            this.BT7.Location = new System.Drawing.Point(15, 60);
+            this.BT7.Name = "BT7";
+            this.BT7.Size = new System.Drawing.Size(75, 23);
+            this.BT7.TabIndex = 3;
+            this.BT7.Text = "7";
+            this.BT7.UseVisualStyleBackColor = true;
+            // 
+            // BT9
+            // 
+            this.BT9.Location = new System.Drawing.Point(177, 60);
+            this.BT9.Name = "BT9";
+            this.BT9.Size = new System.Drawing.Size(75, 23);
+            this.BT9.TabIndex = 4;
+            this.BT9.Text = "9";
+            this.BT9.UseVisualStyleBackColor = true;
+            // 
+            // BT8
+            // 
+            this.BT8.Location = new System.Drawing.Point(96, 60);
+            this.BT8.Name = "BT8";
+            this.BT8.Size = new System.Drawing.Size(75, 23);
+            this.BT8.TabIndex = 5;
+            this.BT8.Text = "8";
+            this.BT8.UseVisualStyleBackColor = true;
+            // 
+            // BTMais
+            // 
+            this.BTMais.Location = new System.Drawing.Point(258, 60);
+            this.BTMais.Name = "BTMais";
+            this.BTMais.Size = new System.Drawing.Size(26, 23);
+            this.BTMais.TabIndex = 6;
+            this.BTMais.Text = "+";
+            this.BTMais.UseVisualStyleBackColor = true;
+            // 
+            // BTMenos
+            // 
+            this.BTMenos.Location = new System.Drawing.Point(258, 100);
+            this.BTMenos.Name = "BTMenos";
+            this.BTMenos.Size = new System.Drawing.Size(26, 23);
+            this.BTMenos.TabIndex = 10;
+            this.BTMenos.Text = "-";
+            this.BTMenos.UseVisualStyleBackColor = true;
+            // 
+            // BT5
+            // 
+            this.BT5.Location = new System.Drawing.Point(96, 100);
+            this.BT5.Name = "BT5";
+            this.BT5.Size = new System.Drawing.Size(75, 23);
+            this.BT5.TabIndex = 9;
+            this.BT5.Text = "5";
+            this.BT5.UseVisualStyleBackColor = true;
+            // 
+            // BT6
+            // 
+            this.BT6.Location = new System.Drawing.Point(177, 100);
+            this.BT6.Name = "BT6";
+            this.BT6.Size = new System.Drawing.Size(75, 23);
+            this.BT6.TabIndex = 8;
+            this.BT6.Text = "6";
+            this.BT6.UseVisualStyleBackColor = true;
+            // 
+            // BT4
+            // 
+            this.BT4.Location = new System.Drawing.Point(15, 100);
+            this.BT4.Name = "BT4";
+            this.BT4.Size = new System.Drawing.Size(75, 23);
+            this.BT4.TabIndex = 7;
+            this.BT4.Text = "4";
+            this.BT4.UseVisualStyleBackColor = true;
+            // 
+            // BTDividir
+            // 
+            this.BTDividir.Location = new System.Drawing.Point(258, 142);
+            this.BTDividir.Name = "BTDividir";
+            this.BTDividir.Size = new System.Drawing.Size(26, 23);
+            this.BTDividir.TabIndex = 14;
+            this.BTDividir.Text = "/";
+            this.BTDividir.UseVisualStyleBackColor = true;
+            // 
+            // BT2
+            // 
+            this.BT2.Location = new System.Drawing.Point(96, 142);
+            this.BT2.Name = "BT2";
+            this.BT2.Size = new System.Drawing.Size(75, 23);
+            this.BT2.TabIndex = 13;
+            this.BT2.Text = "2";
+            this.BT2.UseVisualStyleBackColor = true;
+            // 
+            // BT3
+            // 
+            this.BT3.Location = new System.Drawing.Point(177, 142);
+            this.BT3.Name = "BT3";
+            this.BT3.Size = new System.Drawing.Size(75, 23);
+            this.BT3.TabIndex = 12;
+            this.BT3.Text = "3";
+            this.BT3.UseVisualStyleBackColor = true;
+            // 
+            // BT1
+            // 
+            this.BT1.Location = new System.Drawing.Point(15, 142);
+            this.BT1.Name = "BT1";
+            this.BT1.Size = new System.Drawing.Size(75, 23);
+            this.BT1.TabIndex = 11;
+            this.BT1.Text = "1";
+            this.BT1.UseVisualStyleBackColor = true;
+            this.BT1.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // BTMultiplicar
+            // 
+            this.BTMultiplicar.Location = new System.Drawing.Point(258, 182);
+            this.BTMultiplicar.Name = "BTMultiplicar";
+            this.BTMultiplicar.Size = new System.Drawing.Size(26, 23);
+            this.BTMultiplicar.TabIndex = 18;
+            this.BTMultiplicar.Text = "x";
+            this.BTMultiplicar.UseVisualStyleBackColor = true;
+            // 
+            // BT0
+            // 
+            this.BT0.Location = new System.Drawing.Point(96, 182);
+            this.BT0.Name = "BT0";
+            this.BT0.Size = new System.Drawing.Size(75, 23);
+            this.BT0.TabIndex = 17;
+            this.BT0.Text = "0";
+            this.BT0.UseVisualStyleBackColor = true;
+            // 
+            // BTIgual
+            // 
+            this.BTIgual.Location = new System.Drawing.Point(177, 182);
+            this.BTIgual.Name = "BTIgual";
+            this.BTIgual.Size = new System.Drawing.Size(75, 23);
+            this.BTIgual.TabIndex = 16;
+            this.BTIgual.Text = "=";
+            this.BTIgual.UseVisualStyleBackColor = true;
+            this.BTIgual.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // BTLimpar
+            // 
+            this.BTLimpar.Location = new System.Drawing.Point(15, 182);
+            this.BTLimpar.Name = "BTLimpar";
+            this.BTLimpar.Size = new System.Drawing.Size(75, 23);
+            this.BTLimpar.TabIndex = 15;
+            this.BTLimpar.Text = "Limpar";
+            this.BTLimpar.UseVisualStyleBackColor = true;
+            this.BTLimpar.Click += new System.EventHandler(this.BTLimpar_Click);
+            // 
+            // mtbResultadao
+            // 
+            this.mtbResultadao.Location = new System.Drawing.Point(15, 22);
+            this.mtbResultadao.Name = "mtbResultadao";
+            this.mtbResultadao.Size = new System.Drawing.Size(269, 20);
+            this.mtbResultadao.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 224);
+            this.Controls.Add(this.mtbResultadao);
+            this.Controls.Add(this.BTMultiplicar);
+            this.Controls.Add(this.BT0);
+            this.Controls.Add(this.BTIgual);
+            this.Controls.Add(this.BTLimpar);
+            this.Controls.Add(this.BTDividir);
+            this.Controls.Add(this.BT2);
+            this.Controls.Add(this.BT3);
+            this.Controls.Add(this.BT1);
+            this.Controls.Add(this.BTMenos);
+            this.Controls.Add(this.BT5);
+            this.Controls.Add(this.BT6);
+            this.Controls.Add(this.BT4);
+            this.Controls.Add(this.BTMais);
+            this.Controls.Add(this.BT8);
+            this.Controls.Add(this.BT9);
+            this.Controls.Add(this.BT7);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button BT7;
+        private System.Windows.Forms.Button BT9;
+        private System.Windows.Forms.Button BT8;
+        private System.Windows.Forms.Button BTMais;
+        private System.Windows.Forms.Button BTMenos;
+        private System.Windows.Forms.Button BT5;
+        private System.Windows.Forms.Button BT6;
+        private System.Windows.Forms.Button BT4;
+        private System.Windows.Forms.Button BTDividir;
+        private System.Windows.Forms.Button BT2;
+        private System.Windows.Forms.Button BT3;
+        private System.Windows.Forms.Button BT1;
+        private System.Windows.Forms.Button BTMultiplicar;
+        private System.Windows.Forms.Button BT0;
+        private System.Windows.Forms.Button BTIgual;
+        private System.Windows.Forms.Button BTLimpar;
+        private System.Windows.Forms.MaskedTextBox mtbResultadao;
+    }
+}
+
